2fbb9d31c48b44420f2d9e379379715359f795b9
TusharAtrii <tushar@prologictechnologies.in>
2023-11-16 13:23:09 +0530

    change

--- src/views/components/signUpComp.tsx
+++ src/views/components/signUpComp.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function signUpComp() {
-  return <div>signUpComp</div>;
+  return <div>This is the test for checking the change in done or not.</div>;
 }
 
 export default signUpComp;
