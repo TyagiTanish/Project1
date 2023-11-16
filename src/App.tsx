33fb634fd64a40dd1d2c2db30d32027417b93862
TyagiTanish <tanish@prologictechnologies.in>
2023-11-16 16:53:41 +0530

    Otp verification page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
 import OtpVerification from "./views/components/OtpVerification";
+import MainPage from "./views/pages/MainPage";
 
 function App() {
-  return (<OtpVerification />)
+  return <MainPage />
 }
 
 export default App;
