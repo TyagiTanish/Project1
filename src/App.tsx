c3ab7f3d7171c066146bfe552c2f6dcf84d24469
TusharAtrii <tushar@prologictechnologies.in>
2023-11-16 18:03:35 +0530

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 // import OtpVerification from "./views/components/OtpVerification";
-import SignUpComp from "./views/components/Login";
+import SignUpComp from "./views/components/Loginn";
 function App() {
   // return <OtpVerification />
   return <SignUpComp />;
