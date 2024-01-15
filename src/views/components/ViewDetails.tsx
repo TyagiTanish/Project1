0625c99553786e091e3c96897fc0358b157fa836
TusharAtrii <tushar@prologictechnologies.in>
2024-01-15 14:19:54 +0530

    dynamic hotel and rooms page

--- src/views/components/ViewDetails.tsx
+++ src/views/components/ViewDetails.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 import React from "react";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import WifiIcon from "@mui/icons-material/Wifi";
@@ -10,7 +10,52 @@ import PoolIcon from "@mui/icons-material/Pool";
 import CheckroomIcon from "@mui/icons-material/Checkroom";
 import WineBarIcon from "@mui/icons-material/WineBar";
 import GroupsIcon from "@mui/icons-material/Groups";
-function ViewDetails() {
+import NetworkWifiIcon from "@mui/icons-material/NetworkWifi";
+import DryCleaningIcon from "@mui/icons-material/DryCleaning";
+function ViewDetails({ hotels }: any) {
+  const amenitie = [
+    { id: "parking", label: "Parking", icon: <LocalParkingIcon />, index: "0" },
+    { id: "wifi", label: "Wifi", icon: <NetworkWifiIcon />, index: "1" },
+    { id: "pool", label: "Pool", icon: <PoolIcon />, index: "2" },
+    {
+      id: "roomService",
+      label: "Room Service",
+      icon: <RoomServiceIcon />,
+      index: "3",
+    },
+    { id: "gym", label: "Gym", icon: <FitnessCenterIcon />, index: "4" },
+    {
+      id: "dryClean",
+      label: "DryClean",
+      icon: <DryCleaningIcon />,
+      index: "5",
+    },
+    { id: "bar", label: "Bar", icon: <WineBarIcon />, index: "6" },
+    { id: "meeting", label: "Meeting", icon: <GroupsIcon />, index: "7" },
+    { id: "parking", label: "Parking", icon: <LocalParkingIcon />, index: "8" },
+    { id: "wifi", label: "Wifi", icon: <NetworkWifiIcon />, index: "9" },
+    { id: "pool", label: "Pool", icon: <PoolIcon />, index: "10" },
+    {
+      id: "roomService",
+      label: "Room Service",
+      icon: <RoomServiceIcon />,
+      index: "11",
+    },
+    { id: "gym", label: "Gym", icon: <FitnessCenterIcon />, index: "12" },
+    {
+      id: "dryClean",
+      label: "DryClean",
+      icon: <DryCleaningIcon />,
+      index: "13",
+    },
+    { id: "bar", label: "Bar", icon: <WineBarIcon />, index: "14" },
+    { id: "meeting", label: "Meeting", icon: <GroupsIcon />, index: "15" },
+  ];
+
+  const ShowAmenities = amenitie.filter((v, i) =>
+    hotels[0].amenities[0].includes(+i)
+  );
+
   return (
     <>
       <Stack
@@ -20,90 +65,65 @@ function ViewDetails() {
         margin={4}
         marginLeft={`5%`}
       >
-        <Typography sx={{ fontWeight: "bold", fontSize: {xl:18, md:16,sm:16}}}>
+        <Typography
+          sx={{ fontWeight: "bold", fontSize: { xl: 18, md: 16, sm: 16 } }}
+        >
           Description
         </Typography>
-        <Typography sx={{ fontSize: {xl:14, md:12,sm:11}, letterSpacing: 1 }}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cupiditate
-          aliquid similique error aut eveniet, ad ipsam minus a quo rerum?
-          Numquam sed a ea minus rem voluptatem. Ad, aperiam illum voluptatibus
-          similique magni ratione, consequuntur sint impedit, tempore ab nostrum
-          maiores. Saepe velit dolorem at repellendus nam necessitatibus,
-          repudiandae est aspernatur autem quos, consequatur praesentium eveniet
-          laborum dolor consectetur. Cupiditate beatae deserunt sit rerum id
-          nesciunt aliquam fuga, fugiat in eos, placeat architecto
-          reprehenderit, voluptate quaerat
+        <Typography
+          sx={{ fontSize: { xl: 14, md: 12, sm: 11 }, letterSpacing: 1 }}
+        >
+          <Box
+            dangerouslySetInnerHTML={{ __html: hotels[0]?.discription }}
+            sx={{ flex: 1 }}
+          />
         </Typography>
-        <Stack direction={"row"} spacing={12} sx={{ fontWeight: "bold" ,fontSize:{xl:18,md:18,sm:15}}}>
-          <Typography sx={{fontSize:{xl:20,md:16,sm:15}}}>Check-In</Typography>
-          <Typography sx={{fontSize:{xl:20,md:16,sm:15}}}>Check-Out</Typography>
+        <Stack
+          direction={"row"}
+          spacing={12}
+          sx={{ fontWeight: "bold", fontSize: { xl: 18, md: 18, sm: 15 } }}
+        >
+          <Typography sx={{ fontSize: { xl: 20, md: 16, sm: 15 } }}>
+            Check-In
+          </Typography>
+          <Typography sx={{ fontSize: { xl: 20, md: 16, sm: 15 } }}>
+            Check-Out
+          </Typography>
         </Stack>
-        <Stack direction={"row"} spacing={9} alignItems={'center'}>
+        <Stack direction={"row"} spacing={9} alignItems={"center"}>
           <Stack direction={"row"} spacing={1}>
-            <AccessTimeIcon fontSize="small"/>
-            <Typography  sx={{fontSize:{xl:17,md:15,sm:15}}}>03:00 PM</Typography>
+            <AccessTimeIcon fontSize="small" />
+            <Typography sx={{ fontSize: { xl: 17, md: 15, sm: 15 } }}>
+              03:00 PM
+            </Typography>
           </Stack>
-          <Stack direction={"row"} spacing={1} >
-            <AccessTimeIcon fontSize="small"/>
-            <Typography sx={{fontSize:{xl:17,md:15,sm:15}}}>12:00 PM</Typography>
+          <Stack direction={"row"} spacing={1}>
+            <AccessTimeIcon fontSize="small" />
+            <Typography sx={{ fontSize: { xl: 17, md: 15, sm: 15 } }}>
+              12:00 PM
+            </Typography>
           </Stack>
         </Stack>
       </Stack>
-      <Typography sx={{ fontWeight: "bold", m: "2% 2%  2% 5% ", fontSize: {xl:18, md:16,sm:16}}}>
+      <Typography
+        sx={{
+          fontWeight: "bold",
+          m: "2% 2%  2% 5% ",
+          fontSize: { xl: 18, md: 16, sm: 16 },
+        }}
+      >
         Amenities
       </Typography>
-      
-      <Stack direction={"row"} justifyContent={'space-evenly'} sx={{mb:'2%'}}>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <WifiIcon />
-          <Typography sx={{ fontSize: {xl:15,md:13,sm:12} }}>Fast Internet Access</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <LocalParkingIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Free Parking</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <RestaurantIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>On-site Restaurant</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <RoomServiceIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Room Service</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <FitnessCenterIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Fitness Center</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <PoolIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Pool</Typography>
-        </Stack>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <CheckroomIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Laundry</Typography>
-        </Stack>
-      </Stack>
-      <Stack direction={"row"} sx={{ml:{xl:'8.6%', md:'7%',sm:'5%'}}}>
-        <Stack direction={"column"} spacing={1} alignItems={"center"}>
-          {" "}
-          <GroupsIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Meeting Facilities</Typography>
-        </Stack>
-
-        <Stack direction={"column"} spacing={1} alignItems={"center"} sx={{ml:
-        {xl:'10%',md:'7%',sm:'7%'}}}>
-          {" "}
-          <WineBarIcon />
-          <Typography  sx={{ fontSize: {xl:15,md:13,sm:12} }}>Concierge</Typography>
-        </Stack>
-      </Stack>
+      <Grid container spacing={2} sx={{ mb: 4, ml: { lg: 5, md: 0 } }}>
+        {ShowAmenities.map((item) => (
+          <Grid item xs={2}>
+            <Stack sx={{ fontSize: "10px", ml: 5 }}>
+              <Box sx={{ fontSize: "small" }}>{item.icon}</Box>
+              {item.label}
+            </Stack>
+          </Grid>
+        ))}
+      </Grid>
     </>
   );
 }
