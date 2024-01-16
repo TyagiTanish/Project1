36a2a2297dbdfd87fe2a5badf334c722b734303a
TusharAtrii <tushar@prologictechnologies.in>
2024-01-16 15:22:56 +0530

    addMapinfo

--- src/views/components/Map2.tsx
+++ src/views/components/Map2.tsx
@@ -1,25 +1,64 @@
-import React from "react";
+import React, { useEffect } from "react";
 import GoogleMapReact from "google-map-react";
 import { Box, IconButton } from "@mui/material";
 
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import mapboxgl from "mapbox-gl";
 
-const AnyReactComponent = ({ text }: any) => <div>{text}</div>;
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useSelector } from "react-redux";
+// const AnyReactComponent = ({ text }: any) => <div>{text}</div>;
 export default function SimpleMap() {
-  const defaultProps = {
-    center: {
-      lat: 30.733315,
-      lng: 76.779419,
-    },
-    zoom: 11,
-  };
+  const location = useSelector((state: any) => state.userReducer.location);
+  useEffect(() => {
+    mapboxgl.accessToken =
+      "pk.eyJ1IjoidGFuaXNoLXR5YWdpIiwiYSI6ImNscmV0YWJmcTFocmoybHFpZDQ3dHFkdzMifQ.szsjsVkaiJpDsGUe7LR_4A";
 
-  const handleChange = (value: any) => {
-    console.log(value);
-  };
+    const map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [
+        location?.longitude || 76.779419,
+        location?.latitude || 30.733315,
+      ],
+      zoom: 12,
+    });
+    map.addControl(new mapboxgl.NavigationControl());
+    // Array of marker coordinates
+    const markerCoordinates = [
+      [76.779419, 30.733315],
+      [-74.6, 40.2],
+      [-74.7, 40.3],
+    ];
+
+    // Add markers to the map
+    markerCoordinates.forEach((coord: any) => {
+      new mapboxgl.Marker().setLngLat(coord).addTo(map);
+    });
+    return () => map.remove();
+  }, [location]);
+
+  // const defaultProps = {
+  //   center: {
+  //     lat: 30.733315,
+  //     lng: 76.779419,
+  //   },
+  //   zoom: 11,
+  // };
+
+  // const handleChange = (value: any) => {
+  //   console.log(value);
+  // };
   return (
-    <Box sx={{ height: "200px", minWidth: { sm: "48vw", lg: "35vw" } }}>
-      <GoogleMapReact
+    <Box
+      id="map"
+      sx={{ height: "200px", minWidth: { sm: "48vw", lg: "35vw" } }}
+    />
+  );
+}
+// <Box sx={{ height: "200px", minWidth: { sm: "48vw", lg: "35vw" } }}>
+{
+  /* <GoogleMapReact
         bootstrapURLKeys={{ key: "" }}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
@@ -34,59 +73,7 @@ export default function SimpleMap() {
             </IconButton>
           }
         />
-      </GoogleMapReact>
-    </Box>
-  );
+      </GoogleMapReact> */
 }
 
-// import React from "react";
-// import GoogleMapReact from "google-map-react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { userLocation } from "./redux/user/userSlice";
-// import LocationOnIcon from "@mui/icons-material/LocationOn";
-// import { IconButton } from "@mui/material";
-
-// const AnyReactComponent = ({ text }: any) => <div>{text}</div>;
-
-// export default function SimpleMap() {
-//   const location = useSelector((state: any) => state.userReducer.location);
-//   const dispatch = useDispatch();
-//   const defaultProps = {
-//     center: {
-//       lat: location?.latitude,
-//       lng: location?.longitude,
-//     },
-//     zoom: 16,
-//   };
-//   const handleChange = (value: any) => {
-//     console.log(value.center);
-
-//     const data: any = {
-//       latitude: value.center.lat,
-//       longitude: value.center.lng,
-//     };
-//     dispatch(userLocation(data));
-//   };
-//   return (
-//     <div style={{ height: "200px", minWidth: "48vw" }}>
-//       <GoogleMapReact
-//         bootstrapURLKeys={{ key: "" }}
-//         defaultCenter={defaultProps.center}
-//         defaultZoom={defaultProps.zoom}
-//         onChange={(value) => handleChange(value)}
-//         center={defaultProps.center}
-//         zoom={defaultProps.zoom}
-//       >
-//         <AnyReactComponent
-//           lat={location?.latitude || 30.733315}
-//           lng={location?.longitude || 76.779419}
-//           text={
-//             <IconButton sx={{ color: "red" }}>
-//               <LocationOnIcon />
-//             </IconButton>
-//           }
-//         />
-//       </GoogleMapReact>
-//     </div>
-//   );
-// }
+// </Box>
