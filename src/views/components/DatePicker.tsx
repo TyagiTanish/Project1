f50368f5ba2fecfb35a14426978cbf678d60cdda
TyagiTanish <tanish@prologictechnologies.in>
2024-01-18 13:09:46 +0530

    payment method

--- src/views/components/DatePicker.tsx
+++ src/views/components/DatePicker.tsx
@@ -1,36 +1,13 @@
-// import * as React from "react";
-// import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-// import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+
 import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 import { SingleInputDateRangeField } from "@mui/x-date-pickers-pro/SingleInputDateRangeField";
-// import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-// import dayjs from "dayjs";
-
-// export default function SingleInputDateRangePicker() {
-//   return (
-//     <LocalizationProvider dateAdapter={AdapterDayjs}>
-//       <DemoContainer components={["SingleInputDateRangeField"]}>
-//         <DateRangePicker
-//           slots={{ field: SingleInputDateRangeField }}
-//           sx={{ bgcolor: "white" }}
-//           name="allowedRange"
-//           defaultValue={[dayjs(Date.now()), dayjs(Date.now()+1)]}
-
-//         />
-//       </DemoContainer>
-//     </LocalizationProvider>
-//   );
-// }
-
 import * as React from "react";
 import dayjs from "dayjs";
-import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { DemoContainer} from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-
-import { StaticDateRangePicker } from "@mui/x-date-pickers-pro/StaticDateRangePicker";
 import { pickersLayoutClasses } from "@mui/x-date-pickers/PickersLayout";
-import { DesktopDateRangePicker } from "@mui/x-date-pickers-pro/DesktopDateRangePicker";
+
 
 const current = new Date();
 const date = `${current.getDate()}-${
