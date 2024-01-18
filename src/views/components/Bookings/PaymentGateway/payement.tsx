d6957272e874e3b1f6965c1104e96b7cb07120cd
TyagiTanish <tanish@prologictechnologies.in>
2024-01-18 15:02:24 +0530

    paymentGateway

--- src/views/components/Bookings/PaymentGateway/payement.tsx
+++ src/views/components/Bookings/PaymentGateway/payement.tsx
@@ -1,43 +1,55 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import OyoLogo from '../../../../assets/OyoLogo';
-const PayByRazorPay = () => {
-    const options = {
-        key: 'rzp_test_uic91cd9JLYdsr',
-        amount: '100', //  = INR 1
-        name: 'OYO',
-        description: 'some description',
-        image:<OyoLogo/>,
-        handler: function(response:any) {
-            alert(response.razorpay_payment_id);
-        },
-        prefill: {
-            name: 'Gaurav',
-            contact: '9999999999',
-            email: 'demo@demo.com'
-        },
-        notes: {
-            address: 'some address'
-        },
-        
+import RenderRazorpay from './RenderRazorpay';
+import useAuth from '../../../../Hooks/useAuth/useAuth';
+const Booking=() => {
+    const {request} = useAuth();
+    const [displayRazorpay, setDisplayRazorpay] = useState(false);
+    const [orderDetails, setOrderDetails] = useState({
+      orderId: null,
+      currency: null,
+      amount: null,
+     });
+    
+    const handleCreateOrder = async (amount:any, currency:any) => {
+      const data:any = await request.post('/bookingRequest',
+       {
+        amount: amount*100, //convert amount into lowest unit. here, Dollar->Cents
+        currency,
+        keyId: process.env.key_id,
+        KeySecret: process.env.key_secret,
+       }
+      );
+    
+      if(data && data?.order_id){
+        setOrderDetails({
+          orderId: data.order_id,
+          currency: data.currency,
+          amount: data.amount,
+        });
+        setDisplayRazorpay(true);
     };
-
-    const openPayModal = () => {
-        const rzp1 = new (window as any).Razorpay(options as any);
-        rzp1.open()
-    };
-    useEffect(() => {
-        const script = document.createElement('script');
-        script.src = 'https://checkout.razorpay.com/v1/checkout.js';
-        script.async = true;
-        document.body.appendChild(script);
-    }, []);
-
+}
     return (
-        <>
-            <button onClick={openPayModal}>Pay with Razorpay</button>
-        </>
+      <div>
+        <button 
+          onClick={() => handleCreateOrder(100, 'INR')}
+        >Place Order
+        </button>
+    
+      {displayRazorpay && (
+      <RenderRazorpay
+        amount={orderDetails.amount}
+        currency={orderDetails.currency}
+        orderId={orderDetails.orderId}
+        keyId={process.env.REACT_APP_RAZORPAY_KEY_ID}
+        keySecret={process.env.REACT_APP_RAZORPAY_KEY_SECRET}
+      />
+      )}
+      </div>
     );
 };
 
-export default PayByRazorPay;
+
+export default Booking;
