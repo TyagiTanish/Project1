5b1cc62bdf81f107560d6fcc9c9345392c631a41
TyagiTanish <tanish@prologictechnologies.in>
2024-01-22 15:34:33 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
     <>
       <Locales>
         <SnackbarProvider>
-          {/* <LoginSystem /> */}
+          <LoginSystem />
           {/* <Billing /> */}
           {/* <PayementPage/> */}
           {/* <AllRooms/> */}
