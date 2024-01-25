66f9433942f3ca7637b33e80032e7e17ddfa2933
TyagiTanish <tanish@prologictechnologies.in>
2024-01-25 11:54:31 +0530

    changes

--- src/views/components/Bookings/PaymentGateway/PaymentPage.tsx
+++ src/views/components/Bookings/PaymentGateway/PaymentPage.tsx
@@ -5,12 +5,12 @@ import PaymentForm from "./PaymentForm";
 import { Box } from "@mui/system";
 import { Card, CardContent } from "@mui/material";
 
-const Booking = ({setDisplayLoader,setDisplay,bookingId,totalPrice}:any) => {
+const Booking = ({setDisplayLoader,setDisplay,bookingId,totalPrice,result}:any) => {
   return (
     <StripeProvider>
      
           <Box sx={{ width: 400,padding: 2,border:'1px solid lightgray' }}  >
-            <PaymentForm setDisplayLoader={setDisplayLoader} setDisplay={setDisplay} bookingId={bookingId} totalPrice={totalPrice}  />
+            <PaymentForm setDisplayLoader={setDisplayLoader} setDisplay={setDisplay} bookingId={bookingId} totalPrice={totalPrice} result={result}  />
           </Box>
     
     </StripeProvider>
