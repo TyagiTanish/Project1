6fb552b57d74b803ffe830d6a819d2905662996d
Tanish tyagi <tanish@prologictechnologies.in>
2024-03-08 16:12:21 +0530

    changes for redux persist

--- src/views/components/redux/store.tsx
+++ src/views/components/redux/store.tsx
@@ -1,8 +1,28 @@
+// import { configureStore } from "@reduxjs/toolkit";
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // Choose your storage engine
+import rootReducer from "./rootReducer";
 
-export default configureStore({
-  reducer: {
-    userReducer,
-  },
+// export default configureStore({
+//   reducer: {
+//     userReducer,
+//   },
+// });
+
+// store.js
+
+const persistConfig = {
+  key: "root",
+  storage,
+  // Specify the reducers you want to persist
+  whitelist: ["userReducer"], // In this example, we persist the 'userReducer' reducer
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer, // Disable thunk
 });
+
+export default store;
+export const persistor = persistStore(store);
