b824d941e640c99f2e482e41dd48dc462bc866d9
Tanish tyagi <tanish@prologictechnologies.in>
2024-03-14 16:51:42 +0530

    changes

---
+++ src/views/components/SuperAdmin/SuperAdminView/Actions.tsx
@@ -0,0 +1,86 @@
+import { Button, Stack } from "@mui/material";
+import React, { useState } from "react";
+import LoginModal from "./LoginModal";
+import ShowHotelsModal from "./ShowHotelsModal";
+import EditDialog from "../../EditDialog";
+
+const Actions = ({ setShowHotels, members, item }: any) => {
+  const [loginModal, setLogInModal] = useState(false);
+  const [hotelOwner, setHotelOwner] = useState();
+  const [open, setOpen] = React.useState(false);
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [modalHotel, setModalHotel] = React.useState<any>([]);
+
+  const handleSupport = () => {
+    setLogInModal(true);
+    setShowHotels(false);
+  };
+  const handleClick = async (data: any) => {
+    setModalHotel(data);
+    setOpen(true);
+  };
+  const handleClose = (value: string) => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Stack
+        border={"1px solid  rgb(227, 242, 253)"}
+        borderRadius={3}
+        direction={"row"}
+        padding={2}
+        gap={2}
+        m={2}
+        bgcolor={"#F8F8F8"}
+        sx={{
+          "&:hover": {
+            boxShadow: "rgba(149, 157, 165, 0.2) 0px 0px 15px;",
+            // transition:
+            //   "box-shadow 300ms cubic-bezier(.4, 0, .2, 0.5) 0ms; ",
+          },
+          transition: "  box-shadow cubic-bezier(0.4, 0, 0.2, 1) 200ms  ",
+        }}
+      >
+        <Button
+          variant="outlined"
+          onClick={() => {
+            setOpenDialog(true);
+          }}
+        >
+          Edit
+        </Button>
+
+        <Button
+          variant="outlined"
+          onClick={() => {
+            handleSupport();
+            setHotelOwner(item);
+          }}
+        >
+          Support
+        </Button>
+        <Button variant="outlined" onClick={() => setShowHotels(true)}>
+          View Hotels
+        </Button>
+      </Stack>
+      {open && (
+        // view details button
+        <ShowHotelsModal
+          open={open}
+          onClose={handleClose}
+          modalHotel={modalHotel}
+        />
+      )}
+      <LoginModal
+        loginModal={loginModal}
+        setLogInModal={setLogInModal}
+        members={members}
+        hotelOwner={hotelOwner}
+      />
+      <EditDialog open={openDialog} setOpen={setOpenDialog} item={item} />
+    </>
+  );
+};
+
+export default Actions;
